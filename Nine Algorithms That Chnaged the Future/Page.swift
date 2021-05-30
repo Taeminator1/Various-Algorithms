1a1aa6eea27c06db3df6dfa4c434a27f5dc49806
Taemin <dbsxoals689@naver.com>
2021-05-30 14:07:41 +0900

    DataStructure.framework Import, Random Surfer Trick 구현

--- Nine Algorithms That Chnaged the Future/Page.swift
+++ Nine Algorithms That Chnaged the Future/Page.swift
@@ -6,20 +6,23 @@
 //
 
 import Foundation
+import DataStructure
 
 class Page
 {
-    var data: String
-    var incomingPagesCount: Int = 0
-    var authority: Int = 1
+    var urlString: String
     var outcomingPages: [Page] = []
     
-    init(_ data: String) {
-        self.data = data
+    var incomingPagesCount: Int = 0             // for Hyperlink Trick
+    var authority: Int = 1                      // for Authority Trick
+    var referedCount: Int = 0                   // for Random Surfer Trick
+    
+    init(_ urlString: String) {
+        self.urlString = urlString
     }
     
-    init(_ data: String, linked pages: Page...) {
-        self.data = data
+    init(_ urlString: String, linked pages: Page...) {
+        self.urlString = urlString
         
         for page in pages {
             outcomingPages.append(page)
@@ -28,7 +31,7 @@ class Page
         }
     }
     
-    func setOutcomingLinks(_ pages: Page...) {
+    func setOutcomingPages(_ pages: Page...) {
         for page in pages {
             outcomingPages.append(page)
             page.authority += self.authority
@@ -36,21 +39,58 @@ class Page
         }
     }
     
-    func displayIncomingPagesCount() {
-        print("Incoming pages count of \(data): \(self.incomingPagesCount)")
-    }
-    
-    func displayAuthority() {
-        print("Authority of \(data): \(self.authority)")
-    }
-    
     func displayOutcomingPages() {
-        print("list of outcoming pages of \(data): ", terminator: "")
+        print("list of outcoming pages of \(urlString): ", terminator: "")
         for page in outcomingPages {
-            print("<\(page.data)>", terminator: " ")
+            print("<\(page.urlString)>", terminator: " ")
         }
         print("")
     }
     
+    func displayIncomingPagesCount() {
+        print("Incoming pages count of \(urlString): \(self.incomingPagesCount)")
+    }
     
+    func displayAuthority() {
+        print("Authority of \(urlString): \(self.authority)")
+    }
+    
+    func displayReferedCount() {
+        print("Posiibility of \(urlString): \(self.referedCount)")
+    }
+    
+    func randomSurferTrickSimulator(_ pages: [Page], surfPossiblity: Int = 100, repeatNumber: Int) {
+        let pagesCount: Int = pages.count
+        let pageQueue: Queue = Queue<Page>(Node(self))
+        self.referedCount += 1
+        var repeatCount: Int = 1
+        
+        loop: while repeatCount < repeatNumber && !pageQueue.isEmpty() {
+            let popedPage: Page = pageQueue.pop()!.getData()
+            
+            for page in popedPage.outcomingPages {
+                var outComingPage: Page = page
+                if Page.isPossible(surfPossiblity)! {
+                    var tmpPage: Page = pages[Int.random(in: 0 ..< pagesCount)]
+                    while outComingPage.urlString == tmpPage.urlString {
+                        tmpPage = pages[Int.random(in: 0 ..< pagesCount)]
+                    }
+                    
+                    outComingPage = tmpPage
+                }
+                
+                pageQueue.append(Node(outComingPage))
+                outComingPage.referedCount += 1
+                repeatCount += 1
+            }
+        }
+    }
+    
+    static func isPossible(_ possiblity: Int) -> Bool? {
+        if possiblity < 0 || possiblity > 100 {
+            print("possiblity is out of range")
+            return nil
+        }
+        return Int.random(in: 1 ... 100) <= possiblity
+    }
 }
--- Nine Algorithms That Chnaged the Future/main.swift
+++ Nine Algorithms That Chnaged the Future/main.swift
@@ -15,10 +15,10 @@ var page2: Page = Page("page 2")
 var page3: Page = Page("page 3")
 var page4: Page = Page("page 4")
 
-page0.setOutcomingLinks(page1, page2, page3)
+page0.setOutcomingPages(page1, page2, page3)
 page0.displayOutcomingPages()
 
-page3.setOutcomingLinks(page4)
+page3.setOutcomingPages(page4)
 page3.displayOutcomingPages()
 
 print("")
@@ -38,4 +38,68 @@ print("")
 
 for i in 0 ..< pages.count {
     pages[i].displayOutcomingPages()
+}
+
+
+
+var falseCnt: Int = 0
+var trueCnt: Int = 0
+let possiblity: Int = 10
+for _ in 0 ..< 1000 {
+    if Page.isPossible(possiblity)! {
+        trueCnt += 1
+    }
+    else {
+        falseCnt += 1
+    }
+}
+
+print("trueCnt: \(trueCnt)")
+print("falseCnt: \(falseCnt)")
+
+//page0.randomSurferTrickSimulator(pages, surfPossiblity: 90, repeatNumber: 10)
+//for i in 0 ..< pages.count {
+//    pages[i].displayReferedCount()
+//}
+
+
+var p0: Page = Page("page 0")
+var p1: Page = Page("page 1")
+var p2: Page = Page("page 2")
+var p3: Page = Page("page 3")
+var p4: Page = Page("page 4")
+var p5: Page = Page("page 5")
+var p6: Page = Page("page 6")
+var p7: Page = Page("page 7")
+var p8: Page = Page("page 8")
+var p9: Page = Page("page 9")
+var p10: Page = Page("page 10")
+var p11: Page = Page("page 11")
+var p12: Page = Page("page 12")
+var p13: Page = Page("page 13")
+var p14: Page = Page("page 14")
+var p15: Page = Page("page 15")
+
+p0.setOutcomingPages(p1, p2, p3)
+p1.setOutcomingPages(p4)
+p2.setOutcomingPages(p4, p5)
+p3.setOutcomingPages(p5, p9)
+p4.setOutcomingPages(p5)
+p5.setOutcomingPages(p6, p9)
+p6.setOutcomingPages(p7, p8, p9)
+p7.setOutcomingPages(p8)
+p8.setOutcomingPages(p9, p10, p11, p12, p13)
+p9.setOutcomingPages(p14)
+p10.setOutcomingPages(p14)
+p11.setOutcomingPages(p14)
+p12.setOutcomingPages(p14)
+p13.setOutcomingPages(p14)
+p14.setOutcomingPages(p15)
+p15.setOutcomingPages(p0)
+
+var ps: [Page] = [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15]
+p0.randomSurferTrickSimulator(ps, surfPossiblity: 15, repeatNumber: 100000)
+for i in 0 ..< ps.count {
+//    ps[i].displayReferedCount()
+    print("\(i): \(ps[i].referedCount / 1000)")
 }
