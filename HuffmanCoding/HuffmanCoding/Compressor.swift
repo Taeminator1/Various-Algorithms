299387983201bd9b1536ca883aa14735db762d25
Taemin <dbsxoals689@naver.com>
2021-06-15 21:14:14 +0900

    알고리즘별로 프로젝트 분리

---
+++ HuffmanCoding/HuffmanCoding/Compressor.swift
@@ -0,0 +1,123 @@
+//
+//  Compressor.swift
+//  HuffmanCoding
+//
+//  Created by 윤태민 on 6/8/21.
+//
+
+import Foundation
+import DataStructure
+
+struct Compressor {
+    typealias CodesType = [Character: String]
+    var input: String
+    var pNumber: Int        // positional number
+    
+    init(_ input: String, pNumber: Int = 7) {
+        self.input = input
+        self.pNumber = pNumber
+    }
+    
+    var convertedStrInForm: String {
+        let huffmanCodes: CodesType = makeHuffmanCodes()
+        let compressedStr: String = compressStr(huffmanCodes)
+        var res: String = ""
+        
+        // 허프만 코드의 개수
+        res += String(huffmanCodes.count)
+        res += "\n"
+        
+        // 허프만 코드
+        huffmanCodes.forEach { res += "\($0.key)\($0.value) " }
+        res += "\n"
+        
+        // 원래 문자열의 길이
+        res += String(input.count)
+        res += "\n"
+        
+        // 압축된 문자열
+        res += compressedStr
+        
+        return res
+    }
+    
+    // 허프만 코드 구하기
+    private func makeHuffmanCodes() -> CodesType {
+        if input == "" { return [:] }
+        
+        var keyFreqs: Dictionary<Character, Int> = [:]
+        input.forEach {
+            if keyFreqs[$0] == nil {        // 새로운 문자 업데이트
+                keyFreqs.updateValue(1, forKey: $0)
+            }
+            else {                          // 빈도 수 증가
+                keyFreqs[$0]! += 1
+            }
+        }
+        
+        // 각각의 빈도수에 대한 허프만 코드 구하기
+        let priorityQueue: PriorityQueue = PriorityQueue<HuffmanNode>(handler: <)       // 최소 힙
+        keyFreqs.forEach { priorityQueue.insert(data: HuffmanNode($0.1, $0.0)) }
+        
+        while priorityQueue.count != 1 {
+            let left: HuffmanNode = priorityQueue.pop()!.data
+            let right: HuffmanNode = priorityQueue.pop()!.data
+            
+            let tmp = HuffmanNode(left.getValue() + right.getValue())
+            tmp.left = left
+            tmp.right = right
+            priorityQueue.insert(data: tmp)
+        }
+        
+        let root: HuffmanNode = priorityQueue.pop()!.data
+        var huffmanCodes: Dictionary<Character, String> = [:]
+        
+        func DFS(_ start: HuffmanNode, _ str: String) {
+            if start.getKey() != nil {
+                huffmanCodes.updateValue(str, forKey: start.getKey()!)
+            }
+            else {
+                DFS(start.left!, str + "0")
+                DFS(start.right!, str + "1")
+            }
+        }
+        
+        DFS(root, "")
+        
+        return huffmanCodes
+    }
+    
+    // 입력 받은 문자열을 허프만 코드를 이용해 반환
+    func compressStr(_ huffmanCodes: CodesType) -> String {
+        // 입력된 문자열을 허프만 코드를 이용해 변환
+        let binStr: String = input.map { huffmanCodes[$0]! }.reduce("") { $0 + $1 }
+        let binStrCnt: Int = binStr.count
+        
+        // 이진 문자열을 pNumber 개씩 쪼개 십진수로 변경
+        var decimals: [Int] = []
+        var index: Int = 0
+        while index < binStrCnt {
+            decimals.append(Int(binStr[index ..< index + pNumber], radix: 2)!)
+            index += pNumber
+        }
+        
+        // 마지막 원소가 pNumber 개로 구성이 안 될경우 처리
+        if index != binStrCnt {
+            index -= pNumber
+            decimals.removeLast()        // pNumber 자리가 안 된 마지막 원소 제거
+            
+            var tmp: String = binStr[index ..< index + binStrCnt]
+            index = binStrCnt
+            // pNumber 자리가 되도록 뒷 부분을 0으로 채움
+            while index % pNumber != 0 {
+                tmp += "0"
+                index += 1
+            }
+            
+            decimals.append(Int(tmp, radix: 2)!)
+        }
+        
+        // 쪼개진 십진수들을 유니코드(아스키) 문자열로 반환
+        return decimals.map { String(UnicodeScalar($0)!) }.reduce("") { $0 + $1 }
+    }
+}
---
+++ PageRank/PageRank/Page.swift
@@ -0,0 +1,119 @@
+//
+//  Page.swift
+//  PageRank
+//
+//  Created by 윤태민 on 5/28/21.
+//
+
+import Foundation
+import DataStructure
+
+class Page
+{
+    var urlString: String
+    var outcomingPages: [Page] = []
+    
+    var incomingPagesCount: Int = 0             // for Hyperlink Trick
+    var authority: Int = 1                      // for Authority Trick
+    var referedCount: Int = 0                   // for Random Surfer Trick
+    
+    init(_ urlString: String) {
+        self.urlString = urlString
+    }
+    
+    init(_ urlString: String, linked pages: Page...) {
+        self.urlString = urlString
+        
+        for page in pages {
+            outcomingPages.append(page)
+            page.authority += self.authority
+            page.incomingPagesCount += 1
+        }
+    }
+}
+
+extension Page
+{
+    func setOutcomingPages(_ pages: Page...) {
+        for page in pages {
+            outcomingPages.append(page)
+            page.authority += self.authority
+            page.incomingPagesCount += 1
+        }
+    }
+    
+    func displayOutcomingPages() {
+        print("list of outcoming pages of \(urlString): ", terminator: "")
+        for page in outcomingPages {
+            print("<\(page.urlString)>", terminator: " ")
+        }
+        print("")
+    }
+    
+    func displayIncomingPagesCount() {
+        print("Incoming pages count of \(urlString): \(self.incomingPagesCount)")
+    }
+    
+    func displayAuthority() {
+        print("Authority of \(urlString): \(self.authority)")
+    }
+    
+    func displayReferedCount() {
+        print("Posiibility of \(urlString): \(self.referedCount)")
+    }
+    
+    func randomSurferTrickSimulator(_ pages: [Page], surfPossiblity: Int = 100, repeatNumber: Int) {
+        let pagesCount: Int = pages.count
+        let pageQueue: Queue = Queue<Page>(Node(self))
+        self.referedCount += 1
+        var repeatCount: Int = 1
+        
+        loop: while repeatCount < repeatNumber && !pageQueue.isEmpty {
+            let popedPage: Page = pageQueue.pop()!.getData()
+            
+            if Page.isPossible(surfPossiblity)! {                   // Random Surfer
+                var tmpPage: Page = pages[Int.random(in: 0 ..< pagesCount)]
+                var isLinked: Bool = true
+                while isLinked {
+                    isLinked = false
+                    if popedPage.urlString == tmpPage.urlString {   // 현재 페이지를 다시 선택하지 않도록
+                        isLinked = true
+                        tmpPage = pages[Int.random(in: 0 ..< pagesCount)]
+                        continue
+                    }
+
+                    for p in popedPage.outcomingPages {             // 현재 페이지가 참조하는 페이지를 선택하지 않도록
+                        if p.urlString == tmpPage.urlString {
+                            isLinked = true
+                            tmpPage = pages[Int.random(in: 0 ..< pagesCount)]
+                            break
+                        }
+                    }
+                }
+                pageQueue.append(Node(tmpPage))
+                tmpPage.referedCount += 1
+                repeatCount += 1
+                if repeatCount == repeatNumber { break loop }
+                continue
+            }
+            else {
+                for page in popedPage.outcomingPages {
+                    pageQueue.append(Node(page))
+                    page.referedCount += 1
+                    repeatCount += 1
+                    if repeatCount == repeatNumber { break loop }
+                }
+            }
+        }
+    }
+}
+
+extension Page {
+    static func isPossible(_ possiblity: Int) -> Bool? {
+        if possiblity < 0 || possiblity > 100 {
+            print("possiblity is out of range")
+            return nil
+        }
+        return Int.random(in: 1 ... 100) <= possiblity
+    }
+}
