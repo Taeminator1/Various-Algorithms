f7ac77736d8a2fbefe1b3acf9685c1784e12b871
Taemin <dbsxoals689@naver.com>
2021-06-22 20:57:54 +0900

    Insertion Sort 및 Selection Sort 수정

--- Sorting/Sorting/main.swift
+++ Sorting/Sorting/main.swift
@@ -17,31 +17,25 @@ extension Array {
 extension Array where Self.Element : Comparable {
     
     func insertionSorted(by: byType = (<)) -> [Element] {
-        var resArray: [Element] = self
-        
-        for i in 1 ..< resArray.count {
-            var j: Int = i - 1
-            let tmp = resArray[i]
-            
-            while j >= 0 && !by(resArray[j], tmp) {
-                resArray[j + 1] = resArray[j]
-                j -= 1
-            }
-            resArray[j + 1] = tmp
-        }
-        return resArray
+        var res: [Element] = self
+        insertionSortFunction(&res, by)
+        return res
     }
     
     mutating func insertionSort(by: byType = (<)) {
-        for i in 1 ..< self.count {
+        insertionSortFunction(&self, by)
+    }
+    
+    private func insertionSortFunction(_ input: inout [Element], _ by: byType) {
+        for i in 1 ..< input.count {
             var j: Int = i - 1
-            let tmp = self[i]
+            let tmp = input[i]
             
-            while j >= 0 && !by(self[j], tmp) {
-                self[j + 1] = self[j]
+            while j >= 0 && !by(input[j], tmp) {
+                input[j + 1] = input[j]
                 j -= 1
             }
-            self[j + 1] = tmp
+            input[j + 1] = tmp
         }
     }
 }
@@ -50,41 +44,38 @@ extension Array where Self.Element : Comparable {
 extension Array where Self.Element : Comparable {
     
     func selectionSorted(by: byType = (<)) -> [Element] {
-        var resArray: [Element] = self
-        
-        for i in 0 ..< resArray.count {
-            var tmpIndex: Int = i
-            for j in i + 1 ..< resArray.count {
-                tmpIndex = by(resArray[tmpIndex], resArray[j]) ? tmpIndex : j
-            }
-            resArray.swapAt(i, tmpIndex)
-        }
-        
-        return resArray
+        var res: [Element] = self
+        selectionSortFunction(&res, by)
+        return res
     }
     
     mutating func selectionSort(by: byType = (<)) {
-        for i in 0 ..< self.count {
+        selectionSortFunction(&self, by)
+    }
+    
+    private func selectionSortFunction(_ input: inout [Element], _ by: byType) {
+        for i in 0 ..< input.count {
             var tmpIndex: Int = i
-            for j in i + 1 ..< self.count {
-                tmpIndex = by(self[tmpIndex], self[j]) ? tmpIndex : j
+            for j in i + 1 ..< input.count {
+                tmpIndex = by(input[tmpIndex], input[j]) ? tmpIndex : j
             }
-            self.swapAt(i, tmpIndex)
+            input.swapAt(i, tmpIndex)
         }
     }
 }
 
 
 var arr: [Int] = [11, 3, 4, 1, 2, 3, 6, 2, 4, 45, 5]
-print(arr.insertionSorted())
-print(arr)
-arr.insertionSort()
-//print(arr.sorted())
-//print(arr.insertionSorted())
-print(arr)
-//print(arr.insertionSort(by: <))
+
+//print(arr.insertionSorted(by: >))
 //print(arr)
-//arr.sort(by: <#T##(Int, Int) throws -> Bool#>)
+//arr.insertionSort(by: >)
+//print(arr)
+
+//print(arr.selectionSorted(by: >))
+//print(arr)
+//arr.selectionSort(by: >)
+//print(arr)
 
 
 // throws 추가 필요
