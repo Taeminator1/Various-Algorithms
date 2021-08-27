73adf1ff6387ce12bb7557a067300eb42a6edf37
Taemin <dbsxoals689@naver.com>
2021-08-27 23:16:07 +0900

    매일 갈아입어야 하는 속옷에 대한 세탁 시뮬레이션 구현

---
+++ Laundromat/Laundromat/Stuff.swift
@@ -0,0 +1,35 @@
+//
+//  Stuff.swift
+//  Laundromat
+//
+//  Created by 윤태민 on 8/27/21.
+//
+
+//  Structure for clothes
+
+import Foundation
+
+class Stuff {
+    static var sId: Int = 0             // Whenever create a instance, add unique ID.
+    private(set) var id: Int = 0
+    private(set) var washedNumber: Int = 0
+
+    init() {
+        self.id = Stuff.sId
+        Stuff.sId += 1
+    }
+
+    func wash() {
+        washedNumber += 1
+    }
+
+    func displayState() {
+        print("The washed number of Stuff[\(id)]: \(washedNumber)")
+    }
+}
+
+extension Stuff: Equatable {
+    static func == (lhs: Stuff, rhs: Stuff) -> Bool {
+        lhs.id == rhs.id
+    }
+}
