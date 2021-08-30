0f54f3130be41ea215c9633742ef26708c2ef887
Taemin <dbsxoals689@naver.com>
2021-08-30 19:37:22 +0900

    임시 바구니 두 개를 활용해 조건에 상관없이 모든 옷을 균등하게 세탁 가능

--- Laundromat/Laundromat/Simulators.swift
+++ Laundromat/Laundromat/Simulators.swift
@@ -70,7 +70,8 @@ func normalLaundrySimulator(stuffNumber: Int, term: Int, repeatNumber: Int, poss
     return underwears
 }
 
-// Add temporary basket.
+//  Add temporary basket for property in the fucntion
+//  - tmpWashedBasket: Stuff will be kept there at first after washing. If washedBasket is empty, all stuff in the tmpBasket will be moved to the washedBasket.
 func advancedLaundrySimulator1(stuffNumber: Int, term: Int, repeatNumber: Int, possibility: Double = 1.0, isRandom: Bool = true) -> [Stuff] {
     guard stuffNumber >= term else {
         fatalError("Term has to be more than the number of stuff")
@@ -83,14 +84,13 @@ func advancedLaundrySimulator1(stuffNumber: Int, term: Int, repeatNumber: Int, p
     var washedBasket: Array<Stuff> = []             // 세탁된 옷을 넣는 바구니2
     underwears.forEach { washedBasket.append($0) }  // 세탁된 옷을 바구니2에 넣기
     
-    var tmpBasket: Array<Stuff> = []               // 건조된 옷을 보관하는 임시 바구니1
+    var tmpBasket: Array<Stuff> = []                // 세탁된 옷을 보관하는 임시 바구니1
 
     var washedNumber: Int = 0
     while washedNumber < repeatNumber {             // repeatNumber 만큼 세탁 가능
         var day: Int = 0
         while day < term {                          // 세탁을 위한 주기 카운트
-            
-            while washedBasket.isEmpty {               // 바구니2가 비어있을 경우 바구니3에서 가져오기
+            if washedBasket.isEmpty {               // 바구니2가 비어있을 경우, 임시 바구니에서 가져오기
                 washedBasket.append(contentsOf: tmpBasket)
                 tmpBasket.removeAll()
                 washedBasket.sort { $0.preference < $1.preference }         // 선호도에 따라 바구니2 정렬
@@ -100,9 +100,9 @@ func advancedLaundrySimulator1(stuffNumber: Int, term: Int, repeatNumber: Int, p
             if isPossible {                                         // 속옷 입는 날
                 // isRandom에 따라 어떤 속옷을 입을지 선택: 무작위 또는 바구니2의 가장 바깥쪽
                 if isRandom {
-                    let id: Int = Int.random(in: 0 ..< Stuff.sPreference)
+                    let selectedIndex: Int = Int.random(in: 0 ..< Stuff.sPreference)
                     for i in 0 ..< washedBasket.count {                         // 사용할 속옷이 바구니2에 있는지 확인
-                        if washedBasket[i] == underwears[id] {
+                        if washedBasket[i] == underwears[selectedIndex] {
                             laundryBasket.append(washedBasket.remove(at: i))    // 바구니2에서 사용할 속옷 꺼내고, 사용 후 바구니1에 넣기
                             day += 1
                             break
@@ -119,8 +119,8 @@ func advancedLaundrySimulator1(stuffNumber: Int, term: Int, repeatNumber: Int, p
             }
         }
         
-        tmpBasket.append(contentsOf: doTheWash(laundryBasket))     // 세탁한 속옷을 임시 바구니에 넣기
-        laundryBasket.removeAll()                                   // 바구니3에 옮겼으므로 바구니1 비우기
+        tmpBasket.append(contentsOf: doTheWash(laundryBasket))      // 세탁한 속옷을 임시 바구니에 넣기
+        laundryBasket.removeAll()                                   // 임시 바구니에 옮겼으므로 바구니1 비우기
         washedNumber += 1
     }
 
@@ -128,3 +128,74 @@ func advancedLaundrySimulator1(stuffNumber: Int, term: Int, repeatNumber: Int, p
     return underwears
 }
 
+//  Add temporary baskets for properties in the fucntion
+//  - tmpLaundryBasket: Stuff will be kept there at first after using. If the number of stuff in the tmpLaundryBasket is term, all stuff will be moved to the laundryBasket.
+//  - tmpWashedBasket: Stuff will be kept there at first after washing. If washedBasket is empty, all stuff in the tmpWashedBasket will be moved to the washedBasket.
+func advancedLaundrySimulator2(stuffNumber: Int, term: Int, repeatNumber: Int, possibility: Double = 1.0, isRandom: Bool = true) -> [Stuff] {
+    guard stuffNumber >= term else {
+        fatalError("Term has to be more than the number of stuff")
+    }
+    var underwears: [Stuff] = []
+    Stuff.sPreference = 0
+    for _ in 0 ..< stuffNumber { underwears.append(Stuff()) }      // 관리할 옷 추가
+
+    var laundryBasket: Array<Stuff> = []            // 세탁할 옷을 넣는 바구니1
+    var washedBasket: Array<Stuff> = []             // 세탁된 옷을 넣는 바구니2
+    underwears.forEach { washedBasket.append($0) }  // 세탁된 옷을 바구니2에 넣기
+    
+    var tmpLaundryBasket: Array<Stuff> = []         // 세탁할 옷을 보관하는 임시 바구니1
+    var tmpWashedBasket: Array<Stuff> = []          // 세탁된 옷을 보관하는 임시 바구니2
+
+    var washedNumber: Int = 0
+    while washedNumber < repeatNumber {             // repeatNumber 만큼 세탁 가능
+        var day: Int = 0
+        while day < term {                          // 세탁을 위한 주기 카운트
+            let isPossible: Bool = { Double.random(in: 0.0 ..< 1.0) < possibility }()
+            if isPossible {                                                         // 속옷 입는 날
+                // isRandom에 따라 어떤 속옷을 입을지 선택: 무작위 또는 바구니2의 가장 바깥쪽
+                if isRandom {
+                    let selectedIndex: Int = Int.random(in: 0 ..< Stuff.sPreference)
+                    for i in 0 ..< washedBasket.count {                             // 사용할 속옷이 바구니2에 있는지 확인
+                        if washedBasket[i] == underwears[selectedIndex] {
+                            tmpLaundryBasket.append(washedBasket.remove(at: i))     // 바구니2에서 사용할 속옷을 꺼내고, 임시 바구니1에 넣기
+                            if washedBasket.isEmpty {                               // 바구니2가 비어있을 경우, 임시 바구니2에서 가져오기
+                                washedBasket.append(contentsOf: tmpWashedBasket)
+                                tmpWashedBasket.removeAll()
+//                                washedBasket.sort { $0.preference < $1.preference } // 선호도에 따라 바구니2 정렬
+                            }
+                            if tmpLaundryBasket.count == term {                     // 임시 바구니1에 속옷의 개수가 term이 되면 바구니1로 옮기기
+                                laundryBasket.append(contentsOf: tmpLaundryBasket)
+                                tmpLaundryBasket.removeAll()
+                            }
+                            day += 1
+                            break
+                        }
+                    }
+                }
+                else {
+                    tmpLaundryBasket.append(washedBasket.removeLast())              // 바구니2에서 사용할 속옷을 꺼내고, 임시 바구니1에 넣기
+                    if washedBasket.isEmpty {                                       // 바구니2가 비어있을 경우, 임시 바구니2에서 가져오기
+                        washedBasket.append(contentsOf: tmpWashedBasket)
+                        tmpWashedBasket.removeAll()
+//                        washedBasket.sort { $0.preference < $1.preference }         // 선호도에 따라 바구니2 정렬
+                    }
+                    if tmpLaundryBasket.count == term {                             // 임시 바구니1에 속옷의 개수가 term이 되면 바구니1로 옮기기
+                        laundryBasket.append(contentsOf: tmpLaundryBasket)
+                        tmpLaundryBasket.removeAll()
+                    }
+                    day += 1
+                }
+            }
+            else {
+                day += 1
+            }
+        }
+        
+        tmpWashedBasket.append(contentsOf: doTheWash(laundryBasket))                // 세탁한 속옷을 임시 바구니2에 넣기
+        laundryBasket.removeAll()                                                   // 임시 바구니2에 옮겼으므로 바구니1 비우기
+        washedNumber += 1
+    }
+
+    underwears.forEach { $0.displayState() }
+    return underwears
+}
--- Laundromat/Laundromat/main.swift
+++ Laundromat/Laundromat/main.swift
@@ -25,3 +25,8 @@ import Foundation
 //  옷의 종류가 주기의 배수가 되고, 그 날 옷을 입을 확률이 100%:
 //  - 모든 옷을 균등하게 사용 가능
 //print("Total washed number: \(advancedLaundrySimulator1(stuffNumber: 14, term: 7, repeatNumber: 1000, possibility: 1.0, isRandom: false).reduce(0) { $0 + $1.washedNumber })")
+
+//  옷의 종류가 주기의 2배:
+//  - 선호도에 따라 선택할지 무작위로 선택할지에 상관 없이 모든 옷을 균등하게 사용 가능
+//  - 모든 옷을 균등하게 사용 가능
+print("Total washed number: \(advancedLaundrySimulator2(stuffNumber: 14, term: 7, repeatNumber: 10000, possibility: 0.8, isRandom: true).reduce(0) { $0 + $1.washedNumber })")
