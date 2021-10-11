9d1945ac1acdda23aee2fbcf17a84d2a4acb38a5
Taemin <dbsxoals689@naver.com>
2021-10-11 21:50:39 +0900

    Binary Search 구현

--- Searching/Searching/ArrayExtension.swift
+++ Searching/Searching/ArrayExtension.swift
@@ -8,6 +8,8 @@
 import Foundation
 import DataStructure
 
+// MARK:- Sequential Search
+// Time Complexity: 0(n)
 extension Array where Self.Element : Comparable {
     func sequentialSearch(key: Element) -> Int? {
         for i in 0 ..< self.count {
@@ -19,3 +21,57 @@ extension Array where Self.Element : Comparable {
     }
 }
 
+// MARK:- Binary Search of Sorted Array1
+// - Using recursive function.
+// Time Complexity: log(n)
+extension Array where Self.Element: Comparable {
+    func recursiveBinarySearch(key: Element) -> Int? {
+        let array: [Element] = self.sorted()
+
+        return recursiveBinarySearchFunction(array, key, lowIndex: 0, highIndex: array.count - 1)
+    }
+    
+    private func recursiveBinarySearchFunction(_ array: [Element], _ key: Element, lowIndex: Int, highIndex: Int) -> Int? {
+        let tmpIndex: Int = (lowIndex + highIndex) / 2
+        if lowIndex <= highIndex {
+            if array[tmpIndex] == key {
+                return tmpIndex
+            }
+            else if array[tmpIndex] < key {
+                return recursiveBinarySearchFunction(array, key, lowIndex: tmpIndex + 1, highIndex: highIndex)
+            }
+            else {
+                return recursiveBinarySearchFunction(array, key, lowIndex: lowIndex, highIndex: tmpIndex - 1)
+            }
+        }
+        else {
+            return nil
+        }
+    }
+}
+
+// MARK:- Binary Search of Sorted Array2
+// - Using loop.
+// Time Complexity: log(n)
+extension Array where Self.Element: Comparable {
+    func binarySearchUsingLoop(key: Element) -> Int? {
+        let array: [Element] = self.sorted()
+        
+        var lowIndex: Int = 0
+        var highIndex: Int = array.count - 1
+        while lowIndex <= highIndex {
+            let tmpIndex: Int = (lowIndex + highIndex) / 2
+            if array[tmpIndex] == key {
+                return tmpIndex
+            }
+            else if array[tmpIndex] < key {
+                lowIndex = tmpIndex + 1
+            }
+            else {
+                highIndex = tmpIndex - 1
+            }
+        }
+        
+        return nil
+    }
+}
--- Searching/Searching/main.swift
+++ Searching/Searching/main.swift
@@ -12,3 +12,9 @@ print("Hello, World!")
 var arr: [Int] = [27, 10, 12, 20, 25, 13, 15, 22, 15]
 print(arr.sequentialSearch(key: 10) ?? "Fail to search")
 print(arr.sequentialSearch(key: 14) ?? "Fail to search")
+
+var arr2: [Int] = [1, 3, 4, 6, 7, 8, 10, 13, 14]
+print(arr2.recursiveBinarySearch(key: 4) ?? "Fail to search")
+print(arr2.recursiveBinarySearch(key: 5) ?? "Fail to search")
+print(arr2.binarySearchUsingLoop(key: 4) ?? "Fail to search")
+print(arr2.binarySearchUsingLoop(key: 5) ?? "Fail to search")
